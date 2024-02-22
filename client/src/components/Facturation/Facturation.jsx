537fbc434d33b48b7f7c67100836eae045782deb
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2024-02-22 20:47:23 -0300

    elimiar productos, seleccionarlos y fix bugs

--- client/src/components/Facturation/Facturation.jsx
+++ client/src/components/Facturation/Facturation.jsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import style from './Facturation.module.css';
 import InstanceOfAxios from '../../utils/intanceAxios';
 
@@ -9,6 +9,7 @@ const Facturacion = () => {
     code: '',
     cant: 1,
   });
+  const [total, setTotal] = useState ('');
 
   useEffect (() => {
     fetchData ();
@@ -19,54 +20,101 @@ const Facturacion = () => {
     setData (res);
   };
 
-
   const HandlerFacturacion = () => {
     try {
-      let filteredData = data.filter (
-        el => String (el.code) === String (filter.code)
+      let filteredData = data.filter(
+        (el) => String(el.code) === String(filter.code)
       );
-
+  
       if (filteredData.length > 0) {
-        filteredData = filteredData.map (item => ({
+        filteredData = filteredData.map((item) => ({
           ...item,
           unity: filter.cant,
         }));
-
-        let productIndex = List.findIndex (
-          el => String (el.code) === String (filteredData[0].code)
+  
+        let productIndex = List.findIndex(
+          (el) => String(el.code) === String(filteredData[0].code)
         );
-
+  
         if (productIndex >= 0) {
-          // Product already exists, update the unity property
-          List[productIndex].unity += Number (filter.cant);
-          setList ([...List]); // Update the state with the modified List
+          // Product already exists, update the unity property by adding the quantity
+          List[productIndex].unity = Number(List[productIndex].unity) + Number(filter.cant);
+          setList([...List]); // Update the state with the modified List
         } else {
           // Product doesn't exist, add it to the list
-          setList ([...List, ...filteredData]);
+          setList([...List, ...filteredData]);
         }
-          setFilter ({...filter, code: ''});
+        setFilter({ ...filter, code: '' });
       }
     } catch (error) {
-      console.error ('Error handling facturation:', error);
+      console.error('Error handling facturation:', error);
     }
+  };
+  
+
+  const Calculartotal = () => {
+    let totalData = List.map (el => ({
+      ...el,
+      total: el.priceList * el.unity,
+    }));
+
+    // Calculate the overall total by summing up the individual totals
+    let overallTotal = totalData.reduce ((acc, curr) => acc + curr.total, 0);
+    setTotal (overallTotal);
   };
 
   useEffect (
     () => {
       HandlerFacturacion ();
+      Calculartotal ();
     },
     [filter.code]
   );
 
   const handlerFilters = event => {
-   const {name,value} = event.target;
- 
+    const {name, value} = event.target;
 
     setFilter ({
       ...filter,
       [name]: value ? value[0].toUpperCase () + value.slice (1) : '',
     });
   };
+
+  const handlerEditUnity = elemento => {
+    return event => {
+      const newUnity = event.target.value;
+      let productIndex = List.findIndex (
+        el => String (el.code) === String (elemento.code)
+      );
+
+      if (productIndex >= 0) {
+        // Product already exists, update the unity property
+        List[productIndex].unity = Number (newUnity);
+        setList ([...List]); // Update the state with the modified List
+      }
+    };
+  };
+
+  useEffect (
+    () => {
+      handlerEditUnity ();
+      Calculartotal ();
+    },
+    [List,Calculartotal,handlerEditUnity]
+  );
+
+  const handleCheckboxChange = index => {
+    const updatedList = [...List];
+    updatedList[index].selected = !updatedList[index].selected;
+    setList (updatedList);
+  };
+
+  const handleDeleteSelected = () => {
+    // Filter out selected items from the list
+    const updatedList = List.filter (el => !el.selected);
+    setList (updatedList);
+  };
+
   return (
     <div className={style.containProgram}>
       <div className={style.program}>
@@ -114,22 +162,59 @@ const Facturacion = () => {
 
             {List.map ((el, index) => (
               <div className={style.divProduct} key={index}>
-                <p><input type="checkbox" name="" id="" /></p>
+                <p>
+                  <input
+                    type="checkbox"
+                    name=""
+                    id=""
+                    checked={el.selected || false}
+                    onChange={() => handleCheckboxChange (index)}
+                  />
+                </p>
                 <p>{el.code}</p>
                 <p>{el.title}</p>
                 <p>{el.amount}</p>
                 <p>{el.priceCost}</p>
                 <p>{el.priceList}</p>
-                <p>{el.unity}</p>
+                <p>
+                  <input
+                    type="text"
+                    value={el.unity}
+                    onChange={handlerEditUnity (el)}
+                    name="code"
+                  />
+                </p>
               </div>
             ))}
           </div>
         </div>
 
         <div className={style.importe}>
+          <div className={style.trashBox}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-trash"
+              width="60"
+              height="60"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              onClick={() => handleDeleteSelected ()}
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 7l16 0" />
+              <path d="M10 11l0 6" />
+              <path d="M14 11l0 6" />
+              <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+              <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+            </svg>
+          </div>
           <div className={style.importeBox}>
             <span>Importe total</span>
-            <p>$4500</p>
+            <p>${total ? total : 0}</p>
           </div>
         </div>
       </div>
